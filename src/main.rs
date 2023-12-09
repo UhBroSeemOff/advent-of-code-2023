46cc429178c667629604cb577d39ad4ea70389bc
UhBroSeemOff <svobodmii_login@mail.ru>
2023-12-09 22:26:36 +0700

    Replaced input file Added timer for solutions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use std::time::Instant;
+
 use advent_utilities::solve_task;
 use human_panic::{self, setup_panic};
 
 fn main() {
+    let start_timestamp = Instant::now();
     setup_panic!();
     solve_task();
+    let elapsed = start_timestamp.elapsed().as_secs_f32();
+    println!("==========================");
+    println!("Done in {elapsed} seconds!")
 }
